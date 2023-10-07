df323518b79d5ab9e8fcdc7e52bc6c4cf7c71553
Conrado Augusto <127700916+ConradoAugusto@users.noreply.github.com>
2023-10-07 17:07:41 -0300

    addPlus

--- src/componets/Companies/Companies.jsx
+++ src/componets/Companies/Companies.jsx
@@ -1,77 +1,223 @@
 import matilha from "../../assets/matilha.jpg";
 import aum from "../../assets/aum.jpeg";
 import raizes from "../../assets/raizes.jpg";
+import nectar from "../../assets/nectar.jpg";
 import docevidavegana from "../../assets/docevidavegana.jpg";
 import nampim from "../../assets/nampim.jpeg";
 import gorilla from "../../assets/gorilla.png";
+import comciencia from "../../assets/comciencia.png";
 import robos from "../../assets/robos.jpeg";
+import sorvetario from "../../assets/sorvetario.jpeg";
+import beeveg from "../../assets/beeveg.jpeg";
+import mariabonita from "../../assets/mariabonita.jpg";
+import mana from "../../assets/mana.jpg";
+import LilleSweet from "../../assets/LilleSweet.jpg";
+import AmornaMassa  from "../../assets/AmornaMassa.jpg";
 import styles from "./style.module.scss";
 
 export const Companies = () => {
   return (
-      <ul className={styles.listPiracicaba}>
-        <li>
-          <div className={styles.boxImg}>
+    <ul className={styles.listPiracicaba}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://linktr.ee/matilhaveganfood" target="_blank">
             <img src={matilha} alt="Matilha vegan food" />
-          </div>
+          </a>
+        </div>
+        <a href="https://linktr.ee/matilhaveganfood" target="_blank">
           <h1 className="title three Green">Matilha Vegan Food</h1>
-          <p className="title four Green">Bistrô</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Bistrô</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.instagram.com/nectarvegan_/" target="_blank">
+            <img src={nectar} alt="Néctar Vegan" />
+          </a>
+        </div>
+        <a href="https://www.instagram.com/nectarvegan_/" target="_blank">
+          <h1 className="title three Green">Néctar Vegan</h1>
+        </a>
+        <p className="title four Green">Padaria</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.instagram.com/nampin_veg/" target="_blank">
             <img src={nampim} alt="nampim" />
-          </div>
+          </a>
+        </div>
+        <a href="https://www.instagram.com/nampin_veg/" target="_blank">
           <h1 className="title three Green">Nampin</h1>
-          <p className="title four Green">Restaurante vegano</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Restaurante vegano</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a
+            href="https://sites.google.com/view/gorillaveganburger"
+            target="_blank"
+          >
             <img src={gorilla} alt="gorilla" />
-          </div>
+          </a>
+        </div>
+        <a
+          href="https://sites.google.com/view/gorillaveganburger"
+          target="_blank"
+        >
           <h1 className="title three Green">Gorilla Vegan Burger</h1>
-          <p className="title four Green">Restaurante vegano</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Restaurante vegano</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.instagram.com/robosveganos/" target="_blank">
             <img src={robos} alt="robos veganos" />
-          </div>
+          </a>
+        </div>
+        <a href="https://www.instagram.com/robosveganos/" target="_blank">
           <h1 className="title three Green">Robos Veganos</h1>
-          <p className="title four Green">Lanchonete vegano</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Lanchonete vegano</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.instagram.com/docevidavegana/" target="_blank">
             <img src={docevidavegana} alt="doce vida vegana" />
-          </div>
+          </a>
+        </div>
+        <a href="https://www.instagram.com/docevidavegana/" target="_blank">
           <h1 className="title three Green">Doce Vida Vegana</h1>
-          <p className="title four Green">Doces e Salgados</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Doces e Salgados</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.instagram.com/a.u.m_natural/" target="_blank">
             <img src={aum} alt="aum" />
-          </div>
+          </a>
+        </div>
+        <a href="https://www.instagram.com/a.u.m_natural/" target="_blank">
           <h1 className="title three Green">AUM natural</h1>
-          <p className="title four Green">Cosméticos</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
+        </a>
+        <p className="title four Green">Cosméticos</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
 
-        <li>
-          <div className={styles.boxImg}>
+      <li>
+        <div className={styles.boxImg}>
+          <a href="https://www.raizescosmetica.com.br/" target="_blank">
             <img src={raizes} alt="raizes" />
-          </div>
+          </a>
+        </div>
+        <a href="https://www.raizescosmetica.com.br/" target="_blank">
           <h1 className="title three Green">Raízes Cosmética</h1>
-          <p className="title four Green">Cosméticos</p>
-          <span className="title four Green">Piracicaba-SP</span>
-        </li>
-      </ul>
+        </a>
+        <p className="title four Green">Cosméticos</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://espacovivacomciencia.com.br/" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={comciencia} alt="Espaço Viva Com Ciência" />
+          </div>
+        </a>
+        <a href="https://espacovivacomciencia.com.br/" target="_blank">
+          <h1 className="title three Green">Espaço Viva Com Ciência</h1>
+        </a>
+        <p className="title four Green">Espaço de Eventos</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/sorvetario/" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={sorvetario} alt="Sorvetário" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/sorvetario/" target="_blank">
+          <h1 className="title three Green">Sorvetário</h1>
+        </a>
+        <p className="title four Green">Sobremesas</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/cozinhabeeveg" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={beeveg} alt="BEE VEG" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/cozinhabeeveg" target="_blank">
+          <h1 className="title three Green">BEE VEG</h1>
+        </a>
+        <p className="title four Green">Confeitaria</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/mariabonitaalimentacao" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={mariabonita} alt="Maria Bonita Alimentação Vegan" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/mariabonitaalimentacao" target="_blank">
+          <h1 className="title three Green">Maria Bonita Alimentação Vegan</h1>
+        </a>
+        <p className="title four Green">Refeições</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/manavegandoceriartesanal" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={mana} alt="Mana Vegan" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/manavegandoceriartesanal" target="_blank">
+          <h1 className="title three Green">Mana Vegan</h1>
+        </a>
+        <p className="title four Green">Doceria</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/lillesweet.doces" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={LilleSweet} alt="Lille Sweet" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/lillesweet.doces" target="_blank">
+          <h1 className="title three Green">Lille Sweet</h1>
+        </a>
+        <p className="title four Green">Doceria</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+
+      <li>
+        <a href="https://www.instagram.com/amornamassa.piracicaba" target="_blank">
+          <div className={styles.boxImg}>
+            <img src={AmornaMassa} alt="Amor na Massa" />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/amornamassa.piracicaba" target="_blank">
+          <h1 className="title three Green">Amor na Massa</h1>
+        </a>
+        <p className="title four Green">Doces e Salgados</p>
+        <span className="title four Green">Piracicaba-SP</span>
+      </li>
+    </ul>
   );
 };
